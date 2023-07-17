80ba1a92c105ed125b5814cb8b6147c6a01af966
BOULARES Mohamedridha <74650380+ridha-boulares@users.noreply.github.com>
2023-07-17 13:15:25 +0200

    Update cucumber.js

--- cucumber.js
+++ cucumber.js
@@ -1,3 +1,11 @@
-module.exports = {
-  format: [ 'json:cucumber-report.json']
-};
+{
+  "default": {
+    "requireModule": ["ts-node/register"],
+    "require": ["test/features/**/*.js"],
+    "format": [
+      "progress-bar",
+      "json:test/reports/cucumber-report.json",
+      "html:test/reports/cucumber-report.html"
+    ]
+  }
+}
